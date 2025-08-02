6ccec88af59e071cec762c7220fce2a683bec335
Hanyeol Ko <hanyul9785@gmail.com>
2025-08-02 21:28:26 +0900

    [refactor] 생성, 수정일자 요소 공통관리

--- src/main/java/com/khy/authproxy/domain/common/entity/TimeStamp.java
+++ src/main/java/com/khy/authproxy/domain/common/entity/TimeStamp.java
@@ -1,7 +1,24 @@
 package com.khy.authproxy.domain.common.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Embeddable
+@Getter
 public class TimeStamp {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
 }
